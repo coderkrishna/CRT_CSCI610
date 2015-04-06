228a93796d9d365f410f76bcf376fa15af5a9bb1
Pranav Krishnamurthy <pkrishna@nyit.edu>
2015-04-06 19:39:49 -0400

    Moving all the files over

---
+++ Chinese_Remainder/src/chinese_remainder/Main.java
@@ -0,0 +1,20 @@
+package chinese_remainder;
+
+import java.util.*; // Adding in the import statement
+
+/**
+ * @author Pranav Krishnamurthy
+ * @since 6th April 2015
+ * This is the main (driver) class which I am using to call the Prime Factors class as well.
+ * 
+ * 7:39 pm - Moving over to the Git folder on my PC
+ */
+public class Main 
+{    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }   
+}
