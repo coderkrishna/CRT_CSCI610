d9e961a2ae0fac7024db2560dd0559da6b4b7333
Pranav Krishnamurthy <pkrishna@nyit.edu>
2015-04-06 20:00:53 -0400

    Initializing Console I/O

--- Chinese_Remainder/src/chinese_remainder/Main.java
+++ Chinese_Remainder/src/chinese_remainder/Main.java
@@ -6,15 +6,24 @@ import java.util.*; // Adding in the import statement
  * @author Pranav Krishnamurthy
  * @since 6th April 2015
  * This is the main (driver) class which I am using to call the Prime Factors class as well.
- * 
- * 7:39 pm - Moving over to the Git folder on my PC
  */
+
 public class Main 
 {    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) 
     {
-        // TODO code application logic here
+        // Prompting for the user to enter in an integer. 
+        System.out.println("Enter an integer, n."); 
+        
+        // Initialize the scanner
+        Scanner kb = new Scanner(System.in); 
+        
+        // Creating the integer variable from the console input
+        int n = kb.nextInt(); 
+        
+        // Confirming with the user. 
+        System.out.print("Your value of input, n = " + n); 
     }   
 }
