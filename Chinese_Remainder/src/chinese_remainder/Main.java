d69795dc7eefbc44b56e8b9fd1bfcaa5ba0db656
Pranav Krishnamurthy <pkrishna@nyit.edu>
2015-04-15 17:04:45 -0400

    Having the necessary changes made such that: (1) All of the coding is up-to-date and (2) That I can be able to convert the Integer List into an integer array.

--- Chinese_Remainder/src/chinese_remainder/Main.java
+++ Chinese_Remainder/src/chinese_remainder/Main.java
@@ -5,14 +5,14 @@ import java.util.*; // Adding in the import statement
 /**
  * @author Pranav Krishnamurthy
  * 
- * @since 10th April 2015
- * Going to now be optimizing this and will then be going to have either a method
- * or a class to filter out redundant elements in the array. 
+ * @since 15th April 2015
+ * Now will be having the ability for the main class to handle the list and converting
+ * it into an array. 
  */
 
 public class Main 
 { 
-    static List<Integer> factors; 
+    static List<Integer> factors_MainClass; 
     /**
      * @param args the command line arguments - Which are usually 
      * none starting at the execution time. 
@@ -36,10 +36,32 @@ public class Main
             // Confirming with the user. 
             System.out.println("Your value of input, n = " + n); 
         
-            for (Integer integer : PrimeFactors.primeFactors(n))
-            {
-                System.out.println(integer); 
-            }
+            // Making the call here - To break the number into factors. 
+            PrimeFactors.primeFactors(n); 
         }
-    }   
+        
+        // Populating the Integer List in the Main Class file
+        factors_MainClass = PrimeFactors.factors; 
+        
+        // Now making the method call to the be able to convert the List into an integer array. 
+        int[]factors = convertIntegers(factors_MainClass);
+        
+        // Going to print out the array elements. 
+        for (int i = 0; i < factors.length; i++)
+        {
+            System.out.println(factors[i]); 
+        }
+    }
+    
+    public static int[] convertIntegers(List<Integer> factors_MainClass)
+    {
+        int[] ret = new int[factors_MainClass.size()]; 
+        
+        for (int i = 0; i < ret.length; i++)
+        {
+            ret[i] = factors_MainClass.get(i).intValue(); 
+        }
+        
+        return ret; 
+    }
 }
